23b8658d2afcdc80e39acc08904668620d870cab
pavelbure34 <dongbinsuh@gmail.com>
2020-07-20 16:46:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Personal Project I: single page portfolio template.
+
+This is a single page resume page templat for web developers.
+It is made with CSS and HTML and responsive to mobile view.
+Anyone can take this source and customize it.
+
+2020. 7. 20 Alistaire Suh.
---
+++ css/animation.css
@@ -0,0 +1,21 @@
+@keyframes bgcolor {
+    0% {
+        background-color: #45a3e5
+    }
+
+    30% {
+        background-color: #66bf39
+    }
+
+    60% {
+        background-color: #eb670f
+    }
+
+    90% {
+        background-color: #f35
+    }
+
+    100% {
+        background-color: #864cbf
+    }
+}
